1692bc4de15731991388eca55b220f27ce3feb5b
Willem-Jan Boogerd <wjboogerd@users.noreply.github.com>
2025-04-08 12:02:37 +0000

    Use local fonts instead of hosted

--- eleventy.config.js
+++ eleventy.config.js
@@ -36,6 +36,7 @@ export default async function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   // static
   eleventyConfig.addPassthroughCopy({ 'src/static': '/' });
+  eleventyConfig.addPassthroughCopy({ 'node_modules/@divezone/divezone-web-styles/static': '/' });
   // shortcodes
   eleventyConfig.addShortcode('year', shortcodes.year);
   // transform
